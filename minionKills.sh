03cbe0c5e27b083720f1b1659e4de08e48e6544e
L0r3 <macnode@mac.com>
2016-06-24 18:01:27 -0700

    v2.5 minor improvements and member scraper split off

--- minionKills.sh
+++ minionKills.sh
@@ -3,7 +3,7 @@
 clear
 
 ########################################
-####  the100.io Minion Kills v2.2   ####
+####  the100.io Minion Kills v2.5   ####
 #### Scrapes member list from group ####
 ####  Calls Bungie API to get grim  ####
 #### 	  the100:  /u/L0r3          ####
@@ -48,94 +48,24 @@ grimCardCount=${#grimObject[*]}
 currentCard=`echo ${grimObject[$((RANDOM%grimCardCount))]}`
 
 #### MANUAL OVERRIDE IF YOU WANT TO PROCESS SPECIFIC ENEMY ####
-currentCard='203010,TheVex'
+currentCard='207150,Shank'
 
 #### INCLUDE FILE WITH YOUR BUNGIE API KEY ####
 source ${BASH_SOURCE[0]/%minionKills.sh/apiKey.sh}
+source ${BASH_SOURCE[0]/%minionKills.sh/hundredMembers.sh}
 
 #### SEPRATE GRIM CARD ID AND NAME ####
 grimID=`echo $currentCard | sed 's/,.*[^,]*//'`
 grimName=`echo $currentCard | rev | sed 's/,.*[^,]*//' | rev`
 
-#######################################
-#### BEGIN 100 MEMBER LIST SECTION ####
-#######################################
+#### CALL FUNCTION TO SCRAPE THE100 MEMBERS ####
+hundredMembers
 
-#### CHECK IF 100 GROUP ID PARAMETER ENTERED ON LAUNCH
-the100group="$1"
-
-if [ -z "$the100group" ]
-then
-	echo
-	echo  "Please enter a group id when launching"
-    echo  "Usage: ./100members.sh [id number from the100]"
-    echo  "Usage: ./100members.sh 1412"
-    echo
-    exit
-else
-    echo; echo "Processing: https://the100.io/groups/$the100group"
-fi
-
-#### FUNCTION TO READ MEMBERS FROM LOCAL FILE ####
-funcMembExtract ()
-{
-if [ -f "/tmp/membRawC.txt" ]
-then
-  while read line
-  do
-    echo $line | grep -q "Xbox One"
-    if [ $? == 0 ]; then
-    extractUser=`echo "$line" | rev | cut -c 34- | rev | cut -c 21- | sed 's/.*>//'`
-    echo "$extractUser" >> "/tmp/100_users.txt"
-    fi
-  done < "/tmp/membRawC.txt"
-
-fi
-}
-
-echo; echo "Deleting old temporary files"
-rm '/tmp/membRawA.txt'
-rm '/tmp/membRawB.txt'
-rm '/tmp/membRawC.txt'
-rm '/tmp/100_users.txt'
-rm '/tmp/100_usersClean.txt'
-echo
-
-#### NUMER OF MEMBER PAGES TO GET FROM THE100 ###
-memberPages='9'
-
-#### LOOP TO CURL THE100 MEMBER PAGES TO FILE ####
-let pageCnt='0'
-while [ $pageCnt -lt "$memberPages" ]; do
-	let pageCnt=$pageCnt+'1'
-	membGet="https://www.the100.io/groups/$the100group?page=$pageCnt"
-	curl -o "/tmp/membRawA.txt" "$membGet"
-	sed -n '/herokuapp/,$p' "/tmp/membRawA.txt" > "/tmp/membRawB.txt"
-	sed '/the100/ d' "/tmp/membRawB.txt" > "/tmp/membRawC.txt"
-	funcMembExtract
-done
-
-#### CREATE ADDITIONAL MEMBERS FILE WITH %20 REMOVED ####
-sed 's/ /%20/g' "/tmp/100_users.txt" > "/tmp/100_usersClean.txt"
-
-#### DELETE TEMPORARY FILES ####
-rm '/tmp/membRawA.txt'
-rm '/tmp/membRawB.txt'
-rm '/tmp/membRawC.txt'
-
-#### DONE ####
-echo
-echo "Done creating member list"
-echo "Members clean names: '/tmp/100_users.txt'"
-echo "Members web names: '/tmp/100_usersClean.txt'"
-
-#####################################
-#### END 100 MEMBER LIST SECTION ####
-#####################################
-
+#### XBOX OR PSN ####
 selectedAccountType='1'
+
+#### SOURCE OF USERS TO PROCESS (this is produced by scraper) ####
 playerList="/tmp/100_usersClean.txt"
-echo
 
 #### FUNCTION TO SEND USERNAME TO BUNGIE TO GET MEMBER ID ####
 funcMemID ()
@@ -163,13 +93,13 @@ let playerCnt='0'
 while read 'player'; do
 	funcMemID
 	funcGetGrimData
-	echo "$player: $grimStatOne $grimName kills"
 	let groupKills=groupKills+$grimStatOne
+	echo "$player: $grimStatOne $grimName kills (group $the100group: $groupKills)"
 	let playerCnt=playerCnt+1
 	grimArr[$playerCnt]="$grimStatOne,$player"
 done < "$playerList"
 
-#### SORT RESULTS SO HIGHEST GRIM RESULT AT TOP ####
+#### SORT SCORES HIGHEST TO LOWEST ####
 function arrSort 
 { for i in ${grimArr[@]}; do echo "$i"; done | sort -n -r -s ; }
 echo
