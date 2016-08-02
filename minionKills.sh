2c2ccf464b80a20e14c370133a408b6faa9f52c4
L0r3 <macnode@mac.com>
2016-08-02 13:16:46 -0700

    v3.3 Now calls the100.io API!

--- minionKills.sh
+++ minionKills.sh
@@ -1,83 +1,62 @@
 #!/bin/bash
 
-clear
-
 ########################################
-####  the100.io Minion Kills v2.6   ####
-#### Scrapes member list from group ####
-####  Calls Bungie API to get grim  ####
+####   the100.io minionKills v3.3   ####
+####   Call the100 API get members  ####
+####   Call Bungie API get grim     ####
 #### 	  the100:  /u/L0r3          ####
 ####      Reddit:  /u/L0r3_Titan    ####
 ####      Twitter: @L0r3_Titan      ####
 ########################################
 
-#### ENEMY ARRAY ####
-grimCard="\
-201090,Acolyte \
-207100,Captain \
-205150,Centurion \
-205140,Colossus \
-601076,Crota \
-207130,Dreg \
-203120,Goblin \
-203150,Harpy \
-203130,Hobgoblin \
-203160,Hydra \
-201110,Knight \
-205120,Legionary \
-201140,Ogre \
-205130,Phalanx \
-201130,Thrall \
-203140,Minotaur \
-207140,Servitor \
-207150,Shank \
-205110,Psion \
-207120,Vandal \
-201120,Wizard \
-205010,TheCabal \
-207010,TheFallen \
-201010,TheHive \
-203010,TheVex"
+clear
 
-# 2016-06-21 Shank
-# 2016-06-28 Servitor
-# 2016-07-06 Vandal
-# 2016-07-11 Thrall
-
-#701180,TheTaken (different processor needed to extract grim)
-
-#### READ grimCard INTO ARRAY VARIABLE, COUNT # OF OBJECTS IN ARRAY, PICK RANDOM ####
-IFS=' '
-grimObject=($grimCard)
-grimCardCount=${#grimObject[*]}
-currentCard=`echo ${grimObject[$((RANDOM%grimCardCount))]}`
-
-#### MANUAL OVERRIDE IF YOU WANT TO PROCESS SPECIFIC ENEMY ####
-currentCard='201130,Thrall'
+#### UNCOMMENT ONE ####
+#currentCard='207150,Shank'
+#currentCard='207140,Servitor'
+#currentCard='207120,Vandal'
+#currentCard='201130,Thrall'
+#currentCard='201110,Knight'
+currentCard='201140,Ogre'
+#currentCard='205130,Phalanx'
+#currentCard='201090,Acolyte'
+#currentCard='207100,Captain'
+#currentCard='205150,Centurion'
+#currentCard='205140,Colossus'
+#currentCard='601076,Crota'
+#currentCard='207130,Dreg'
+#currentCard='203120,Goblin'
+#currentCard='203150,Harpy'
+#currentCard='203130,Hobgoblin'
+#currentCard='203160,Hydra'
+#currentCard='205120,Legionary'
+#currentCard='203140,Minotaur'
+#currentCard='205110,Psion'
+#currentCard='201120,Wizard'
+#currentCard='205010,TheCabal'
+#currentCard='207010,TheFallen'
+#currentCard='201010,TheHive'
+#currentCard='203010,TheVex'
 
 #### INCLUDE FILE WITH YOUR BUNGIE API KEY ####
-source ${BASH_SOURCE[0]/%minionKills.sh/apiKey.sh}
+source ${BASH_SOURCE[0]/%minionKills.sh/apiKeys.sh}
 source ${BASH_SOURCE[0]/%minionKills.sh/hundredMembers.sh}
 
 #### SEPRATE GRIM CARD ID AND NAME ####
 grimID=`echo $currentCard | sed 's/,.*[^,]*//'`
 grimName=`echo $currentCard | rev | sed 's/,.*[^,]*//' | rev`
 
-#### CALL FUNCTION TO SCRAPE THE100 MEMBERS ####
-hundredMembers
-
 #### XBOX OR PSN ####
 selectedAccountType='1'
 
-#### SOURCE OF USERS TO PROCESS (this is produced by scraper) ####
-playerList="/tmp/100_usersClean.txt"
+#### CALL FUNCTION TO GET THE100 GROUP MEMBERS ####
+hundredMembers
 
-#### FUNCTION TO SEND USERNAME TO BUNGIE TO GET MEMBER ID ####
+#### FUCTION TO SEND GAMERTAG TO BUNGIE TO GET MEMBER ID ####
 funcMemID ()
 {
-sleep 1
 getUser=`curl -s -X GET \
--H "Content-Type: application/json" -H "Accept: application/xml" -H "$authKey" \
+-H "Content-Type: application/json" -H "Accept: application/xml" -H "$authKeyBungie" \
 "https://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/$selectedAccountType/$player/"`
 memID=`echo "$getUser" | grep -o 'membershipId.*' | cut -c 16- | sed 's/displayName.*[^displayName]*//' | rev | cut -c 4- | rev`
 }
@@ -85,34 +64,38 @@ memID=`echo "$getUser" | grep -o 'membershipId.*' | cut -c 16- | sed 's/displayN
 #### FUNCTION TO GET ALL THE DATA FROM A SPECIFIC GRIM CARD ("statNumber":1)  ####
 funcGetGrimData ()
 {
-sleep 1
 grimMinion=`curl -s -X GET \
--H "Content-Type: application/json" -H "Accept: application/xml" -H "$authKey" \
+-H "Content-Type: application/json" -H "Accept: application/xml" -H "$authKeyBungie" \
 "https://www.bungie.net/Platform/Destiny/Vanguard/Grimoire/1/$memID/?single=$grimID"`
 grimStatOne=`echo "$grimMinion" | grep -o 'statNumber":1.*' | sed 's/displayValue.*[^displayValue]*//' | rev | cut -c 5- | sed 's/eulav.*[^eulav]*//' | rev | cut -c 3-`
 }
 
-#### LOOP THOUGH LIST OF MEMBERS, RUN FUNCTIONS TO GET BUNGIE GRIM DATA ####
-let groupKills='0'
+#### LOOP THROUGH MEMBERS TO GET SCORES FROM BUNGIE ####
+echo;echo "#### GET RESULTS FROM BUNGIE ####"
 let playerCnt='0'
-while read 'player'; do
+let groupTotal='0'
+while [ "$playerCnt" -lt "$totalMembers" ]; do
+	player=`echo "${arrMembers[$playerCnt]}"`
+	sleep 1
 	funcMemID
 	funcGetGrimData
-	let groupKills=groupKills+$grimStatOne
-	echo "$player: $grimStatOne $grimName kills (group $the100group: $groupKills)"
+	scorePlayer="$grimStatOne,$player"
+	echo "$playerCnt: $scorePlayer"
 	let playerCnt=playerCnt+1
-	grimArr[$playerCnt]="$grimStatOne,$player"
-done < "$playerList"
+	grimArr[$playerCnt]="$scorePlayer"
+	let groupTotal=groupTotal+grimStatOne
+done
 
 #### SORT SCORES HIGHEST TO LOWEST ####
 function arrSort 
 { for i in ${grimArr[@]}; do echo "$i"; done | sort -n -r -s ; }
-echo
 grimScoresSort=( $(arrSort) )
-printf '%s\n' "${grimScoresSort[@]}"
 
-echo
-echo "Group $the100group: $groupKills $grimName kills"
-echo
+sortList=`printf '%s\n' "${grimScoresSort[@]}" | sed 's/,/ /g' | sed 's/%20/ /g'`
 
+echo; echo; echo "#### GROUP TOTAL $grimName kills: $groupTotal ####"
+
+echo "$sortList"
+
+echo; echo
 exit
